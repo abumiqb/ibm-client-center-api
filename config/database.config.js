07a86e2b974a05f6530bd113124e8113fd76192a
Abubakr <abumiqb@gmail.com>
2018-05-04 16:28:48 +0200

    Added briefing.controller

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     url: 'mongodb://admin:ASGNBMVKKCDRXMVE@sl-us-south-1-portal.22.dblayer.com:38723,sl-us-south-1-portal.21.dblayer.com:38723/compose?authSource=admin&ssl=true'
 }
+
